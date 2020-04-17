7ba3a9e70c866737198248d6c4954e12ecfcfbda
Jonatan Markusson <0202majo@jonkoping.se>
2020-04-17 15:38:39 +0200

    version 3.17 20200417

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace Fyra_i_rad
         {
             InitializeComponent();
         }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush bl = new SolidBrush(Color.Black);
+            SolidBrush wh = new SolidBrush(Color.White);
+            Pen p1 = new Pen(Color.Green, 8);
+
+            for(int x= 0; x< 400; x+=100)
+            {
+                for(int y=0; y< 400; y+=100)
+                {
+                    g.FillRectangle(wh, x, y, 50, 50);
+                    g.FillRectangle(bl, x + 50, y, 50, 50);
+                    g.FillRectangle(wh, x + 50, y + 50, 50, 50);
+                    g.FillRectangle(bl, x, y + 50, 50, 50);
+                }
+
+            }
+        }
     }
 }
